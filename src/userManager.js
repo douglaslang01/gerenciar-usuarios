093d0353cd370a7947d00b0af568c9014d6ab380
douglaslang01 <douglas.lang@outlook.com>
2025-09-04 22:37:04 -0300

    Some validations were added to the user manager functions and new unit tests were created to cover this validations.

--- src/userManager.js
+++ src/userManager.js
@@ -5,10 +5,14 @@ function getUsers() {
 }
 
 function createUser(user) {
+    validateUser(user);
+
     users.push(user);
 }
 
 function updateUser(oldUser, newUser) {
+    validateUser(newUser);
+
     const index = users.indexOf(oldUser);
     if (index > -1) {
         users[index] = newUser;
@@ -26,5 +30,16 @@ function deleteUsers() {
     users.length = 0;
 }
 
+validateUser = (user) => {
+    if (user === undefined || user === null || user.trim() === '') {
+        throw new Error('Invalid user');
+    }
+
+    if (users.includes(user)) {
+        throw new Error('User already exists');
+    }
+
+    return true;
+}
 
 module.exports = { getUsers, createUser, updateUser, deleteUser, deleteUsers };
--- test/userManager.test.js
+++ test/userManager.test.js
@@ -9,12 +9,24 @@ describe('User Manager', function () {
         expect(users).to.includes('mathues', 'marcos', 'lucas', 'joão');
     });
 
-    it('should create a new user', function () {
+    it('should create a valid user', function () {
         userManager.createUser('ana');
         const users = userManager.getUsers();
 
         expect(users).to.include('ana');
         expect(users).to.have.lengthOf(5);
+    });
+
+    it('should throw an error when creating an invalid user', function () {
+        const invalidUsers = ['', null, undefined, '   '];
+
+        invalidUsers.forEach(invalidUser => {
+            expect(() => userManager.createUser(invalidUser)).to.throw('Invalid user');
+        });
+    });
+
+    it('should throw an error when creating a duplicate user', function () {
+        expect(() => userManager.createUser('mathues')).to.throw('User already exists');
     });
 
     it('should update an existing user', function () {
@@ -25,11 +37,31 @@ describe('User Manager', function () {
         expect(users).to.not.include('ana');
     });
 
+    it('should not update a non-existing user', function () {
+        const usersBefore = userManager.getUsers();
+        userManager.updateUser('nonexistent', 'newname');
+        const usersAfter = userManager.getUsers();
+
+        expect(usersAfter).to.deep.equal(usersBefore);
+    });
+
+    it('should throw an error when updating to a duplicate user', function () {
+        expect(() => userManager.updateUser('ana maria', 'mathues')).to.throw('User already exists');
+    });
+
     it('should delete an user', function () {
         userManager.deleteUser('ana maria');
         const users = userManager.getUsers();
 
         expect(users).to.not.include('ana maria');
+    });
+
+    it('should not delete a non-existing user', function () {
+        const usersBefore = userManager.getUsers();
+        userManager.deleteUser('nonexistent');
+        const usersAfter = userManager.getUsers();
+
+        expect(usersAfter).to.deep.equal(usersBefore);
     });
 
     it('should delete all users', function () {
